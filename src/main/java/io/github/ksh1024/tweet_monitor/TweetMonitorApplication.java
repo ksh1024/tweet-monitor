144646c18c5549e1c63fb1f09b31c6ed25377a1e
kim <ksh251024@naver.com>
2025-06-23 11:19:43 +0900

    init

---
+++ src/main/java/io/github/ksh1024/tweet_monitor/TweetMonitorApplication.java
@@ -0,0 +1,13 @@
+package io.github.ksh1024.tweet_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetMonitorApplication.class, args);
+	}
+
+}
